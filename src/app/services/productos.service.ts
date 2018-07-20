e0cc69de82845fe4bc1074dfc9707514f8872696
Miguel Higuera <higueraprieto1981@gmail.com>
2018-07-20 13:58:07 -0500

    Página de producto y ruta lista

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -19,9 +19,13 @@ export class ProductosService {
   	this.cargando = true;
   	this.http.get('https://paginaweb-ba4c3.firebaseio.com/productos_idx.json')
   	.subscribe(res => {
-  		console.log( res.json());
-  		this.cargando = false;
-  		this.productos = res.json();
-  	})
+  		//console.log( res.json());
+
+  		//setTimeout( ()=>{
+  			this.cargando = false;
+  			this.productos = res.json();
+  		//},1500) 	
+
+  	});
   }
 }
